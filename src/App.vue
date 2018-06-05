a65aecb37361af68cb97c7e38e32f6b10b9216dc
LittleElijah <cuiyeqing960904@gmail.com>
2018-06-06 01:44:07 +0300

    bringing in two main components and register them

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <h1>Word Translator</h1>
+    <h5>Powered By Vue.js</h5>
+    <TranslateForm></TranslateForm>
   </div>
 </template>
 
 <script>
+import TranslateForm from './components/TranslateForm'
+import TranslateOutput from './components/TranslateOutput'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    TranslateForm, TranslateOutput
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background:#fefefe;
 }
 </style>
