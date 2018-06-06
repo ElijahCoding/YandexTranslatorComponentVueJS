10c6958ecd2b68b5d9faf7cfad5f604f7702f42d
LittleElijah <cuiyeqing960904@gmail.com>
2018-06-06 11:41:48 +0300

    basic TranslateForm set up

--- src/components/TranslateForm.vue
+++ src/components/TranslateForm.vue
@@ -1,9 +1,35 @@
 <template>
-
+  <div id="translateForm">
+    <form @submit="formSubmit">
+      <input type="text"
+      v-model="textToTranslate"
+      placeholder="Enter a Word" />
+      <input type="submit" value="Translate">
+    </form>
+  </div>
 </template>
 
 <script>
   export default {
-    
+    name: 'translateForm',
+
+    data () {
+      return {
+        textToTranslate: ''
+      }
+    },
+
+    methods: {
+      formSubmit (e) {
+        e.preventDefault();
+      }
+    }
   }
 </script>
+
+<style>
+  #transForm{
+    border-radius:10px;
+    border: 1px #ccc solid;
+  }
+</style>
